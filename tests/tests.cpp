351d9f33c04cd9554997d1f9e0fd1f9462a39979
Jules Fouchy <jules.fouchy@ntymail.com>
2023-10-20 05:55:02 +0200

    🚨 Fix tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,6 +4,6 @@ auto main() -> int
 {
     // bool const is_ci = argc > 1 && strcmp(argv[1], "-CI") == 0; // NOLINT(*pointer-arithmetic)
 
-    auto p1 = RtAudioW::Player{}; // This will assert if no API is available, which is something we want to detect.
+    RtAudioW::Player player{}; // This will assert if no API is available, which is something we want to detect.
     return 0;
 }
